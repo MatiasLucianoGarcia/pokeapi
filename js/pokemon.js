1bc29d3a384eb68d1907d4e8ca107dc03dfaa367
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2020-06-17 11:53:43 -0300

    Finish basic pokedex.

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,5 +1,5 @@
 const  poke_container= document.getElementById('poke_container');
-const pokemon_number = 150;
+const pokemon_number = 151;
 const colors = {
     fire : '#FDDFDF',
     grass: '#DEFDE0',
@@ -42,13 +42,16 @@ function createPokemonCard(pokemon){
     const name = pokemon.name[0].toUpperCase()+ pokemon.name.slice(1);
     const poke_types = pokemon.types.map(elem => elem.type.name);
     const type = main_types.find(type=>poke_types.indexOf(type)>-1);
+    const color = colors[type];
+
+    pokemonEl.style.backgroundColor=color;
 
     const pokeInnerHTML = `  
         <div class="img-container">
             <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
         </div>
         <div class="info">
-            <span class="number">${pokemon.id}</span>
+            <span class="number">#${pokemon.id.toString().padStart(3,'0')}</span>
             <h3 class="name">${name}</h3>
             <small class="type">Type:<span>${type}</span></small>
         </div>
