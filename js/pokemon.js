5aea035d6f771f875814ae63fdc4b2bccd937e9c
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2020-06-16 22:29:10 -0300

    Fetching all pokemons

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,11 +1,32 @@
 const  poke_container= document.getElementById('poke_container');
 const pokemon_number = 150;
 
+const fetchPokemons = async ()=>{
+    for (let index = 1; index <= pokemon_number; index++) {
+        await getPokemon(index);
+    }
+}
+
 const getPokemon = async id =>{
     const url= `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
     const pokemon = await res.json();
-    console.log(pokemon);
+    createPokemonCard(pokemon);
 }
 
-getPokemon(1);
+fetchPokemons();
+
+
+function createPokemonCard(pokemon){
+    const pokemonEl= document.createElement('div');
+    pokemonEl.classList.add('pokemon');
+
+    const pokeInnerHTML = `
+        ${pokemon.id}
+    `
+    pokemonEl.innerHTML = pokeInnerHTML;
+    poke_container.appendChild(pokemonEl);
+}
+
+
+
